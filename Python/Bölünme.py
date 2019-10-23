9e9a446778e74d0a5fb2d6addcd19154b95ae34e
aKstarx1 <56915536+aKstarx1@users.noreply.github.com>
2019-10-23 20:18:00 +0300

    Update Bölünme.py

--- Python/Bölünme.py
+++ Python/Bölünme.py
@@ -6,7 +6,12 @@ if a%2==0:
    print(a,"sayısı çifttir.")
 else:
    print(a, "sayısı tektir.")
-
+   
 #GÖREV:Yukarıda verilen örenğin benzerini üçe bölünebilme ile yapınız:
 #!!!Python programlama dilinde boşluklar programın çalışması için önem arz etmektedir.
 #Yukarıdaki örneği baz alarak görevi yerine getirebilirsiniz.
+
+if b%3==0:
+   print(b, "sayısı 3'ün katıdır.")
+else:
+   print(b, "sayısı 3'ün katı değildir.")
